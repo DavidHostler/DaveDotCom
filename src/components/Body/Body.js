954564fc65b95df9b7ccad38f144d710df346c61
David Hostler <47288116+DavidHostler@users.noreply.github.com>
2022-01-01 23:36:41 -0500

    Create Body.js

---
+++ src/components/Body/Body.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Body = () => {
+    return (
+        <div>
+            <h1>Welcome to My Personal Site!</h1>
+            <h2>About Me</h2>
+            <h3>
+I am a developer from Toronto, Canada with a background in undergraduate physics
+at the University of Toronto.
+My interests since graduating in 2020 have been mainly tech-focused, with a focus on backend and
+fullstack development. Along the way I've picked up knowledge about machine learning techniques, 
+and data structures/algorithms. 
+
+Additionally, I dabble in fitness and extreme metal music. I'll have a personal interest page up soon 
+to reflect some of these so that I can prove to any potential employer or client that I am in fact not 
+a time-travelling robot sent here to advertise extended warranty on personal vehicles.
+            
+            </h3>
+        </div>
+    )
+}
+
+export default Body
