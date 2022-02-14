fd893fbf4d6b013938021be8ce3e72fe8044db18
David <davidhos117@gmail.com>
2022-02-14 04:04:51 -0500

    Personal Site Updated

--- src/App.js
+++ src/App.js
@@ -1,30 +1,124 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Outlet,
+  useParams
+} from 'react-router-dom';
+
+
 import "./App.css";
 import Header from "./components/Header/Header";
 import HeaderButton from "./components/HeaderButtons/HeaderButton";
+import ProjectScreen from './screens/ProjectsScreen';
 import Background from "./Background";
+import ProjectsScreen from "./screens/ProjectsScreen";
+import AboutMe from './screens/AboutMe';
 
-function App() {
+import Typist from 'react-typist';
+
+
+
+function Home() {
   return (
-    <div>
-      <Header/>
-      <HeaderButton title={'About Me'}></HeaderButton>
-      <HeaderButton title={'Personal Projects'}></HeaderButton>
-      <HeaderButton title={'Cool Stuff'}></HeaderButton>
-
-      <Background />
-      {/* <CenterTitle /> */}
+    <div style={{ padding: 20 }}>
+      <h2>Home View</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
     </div>
   );
 }
 
-// function Cent() {
+
+function App() {
+  return (
+    <Router>
+      {/* <Header/> */}
+      <nav style={{ margin: 10 }}>
+        <ul>
+        <Link to="/" style={{ padding: 5 }}>
+          Home
+        </Link>
+        <Link to="/Projects" style={{ padding: 5 }}>
+          <HeaderButton title="Projects">
+          Projects  
+          </HeaderButton>
+        </Link>
+        <Link to="/AboutMe" style={{ padding: 5 }}>
+        <HeaderButton title="About Me">
+          About Me
+          </HeaderButton>
+        </Link>
+        </ul>
+      </nav>
+      {/* Rest of the code remains same */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Projects" element={<ProjectsScreen />} />
+        <Route path="/AboutMe" element={<AboutMe />}>
+          {/* <Route path=":slug" element={<Post />} /> */}
+        </Route>
+      </Routes>
+      <Background/>
+    </Router>
+  );
+}
+
+export default App;
+
+
+// import "./App.css";
+// import Header from "./components/Header/Header";
+// import HeaderButton from "./components/HeaderButtons/HeaderButton";
+// import ProjectScreen from './screens/ProjectsScreen';
+// import Background from "./Background";
+// import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
+// import { Switch } from 'react-switch';
+// import ProjectsScreen from "./screens/ProjectsScreen";
+
+// function App() {
 //   return (
-//     <div id="text_div center_all">
-//       <div className="center_all">
-//         <h1 className="custom-subTitle"></h1>
-//       </div>
+//     <div className="App">
+//       <Header/>
+//     <ul>
+//       <li>
+//     <Link to="/ProjectsScreen">
+//     <button>Projects</button>    
+//     </Link>
+//       </li>
+
+//     <Switch>
+//       <Router exact path ='/ProjectsScreen' component={ProjectsScreen}></Router>
+//     </Switch>
+//     </ul>
+    
+//     <Background/>
+
+// {/* 
+//       <Header/>
+//       <HeaderButton title={'About Me'}></HeaderButton>
+//       <HeaderButton title={'Personal Projects'}></HeaderButton>
+//       <HeaderButton title={'Cool Stuff'}></HeaderButton>
+
+//       <Background /> */}
+//       {/* <CenterTitle /> */}
 //     </div>
+
+
+
 //   );
 // }
 
-export default App;
+// // function Cent() {
+// //   return (
+// //     <div id="text_div center_all">
+// //       <div className="center_all">
+// //         <h1 className="custom-subTitle"></h1>
+// //       </div>
+// //     </div>
+// //   );
+// // }
+
+// export default App;
---
+++ src/screens/ProjectsScreen.js
@@ -0,0 +1,141 @@
+import classes from './css/ProjectsScreen.module.css';
+
+const ProjectsScreen = () => {
+    return(
+        <div >
+    <div>
+        <h1 className="custom-subTitle"></h1>
+    <div>
+        <h1>Welcome to Projects</h1>
+    </div>
+        <div><h2>Here is a list of some things that I've worked on!</h2></div>
+        
+        <div><h5>My Github: https://github.com/DavidHostler</h5></div>
+
+        <div><h2>Machine Learning Related Projects</h2></div>
+        <h5>Here are some examples of ML projects that I've created 
+            in 2020, 2021:
+        
+        </h5>
+   
+        <h5>
+            "Milk" (it's a weird name I know, but some people like a nice glass of Milk now and then).
+            This is a super resolution generative adversarial network that  receives an image of a given
+            size, and uses deep learning to "hallucinate" a more high resolution version of that image.            
+            
+        </h5>
+            <div>
+                <h5>
+                https://github.com/DavidHostler/Milk
+                </h5>
+            </div>
+
+            <div><h5>
+                
+            https://github.com/DavidHostler/Pytorch-Gaussian-NN
+
+                </h5></div>
+        <h5>
+
+            This is a little research project I worked on briefly in late 2020 and early 2021.
+            I had volunteered to help some grad students in the University of Toronto Scarborough 
+            materials science research project with implementing a single hidden-layer neural network
+            in order to model high-precision Gaussian functions given a single input value.
+
+            The idea is that if a given element or compound has a known excitation energy known to a 
+            certain accuracy, then we could improve this accuracy by using a neural network to approximate
+            the Gaussian distribution function centred around this excitation energy eigenvalue.
+
+            The results were that the required neural architecture was insufficient for such a desired result,
+            since training the neural network on successive eigenvalue training data had the effect of causing the 
+            model to "unlearn" anything it knew of previous eigenvalue distributions.
+            Therefore, I posit that the only way to implement this method successfully would be to ascribe to the
+            model an attention window- e.g. RNN's, LSTM's or preferably Transformers.
+
+            These models were deemed too complicated for the desired architecture and so they decided to implement
+            the energy distribution functions directly by hard-coding the distribution function.
+
+        </h5>
+
+            <div><h3>DRACO</h3></div>
+           <div> <h5>https://github.com/DavidHostler/Draco</h5> </div>
+        <h5>
+            This one is a favorite for a couple of reasons. I was attempting to implement 
+            a Reinforcement Learning agent using a technique called Deep Deterministic Policy Gradients
+            or DDPG for short. I wanted to maximize the speed of my agent as well.
+            
+            Python is known to run about ~ 20 times more slowly than C/C++, 
+            so I figured that I'd implement my solution in C++ using my current understanding 
+            of the Python code at the time.
+
+            In doing so,  I ran into a few build issues with the Tensorflow C API and figured that
+            since DDPG only uses deep hidden layers (i.e. linear transformations of the form 
+            Ax + b = y, where A is a matrix a.k.a. "weights"", x is the input tensor, b is the bias)
+            
+            This project allowed me to play around with a super fast RL simulation and learn more 
+            about the direct behaviour of deep learning models from a mathematical point of view, 
+            as I had to hard-code the linear transformations and take the derivatives of activation 
+            functions numerically.
+
+            If using this model, depending on the user's architecture you might want to consider implementing
+            a "Big Float" datatype to prevent "Nan" values as weights, biases or gradients.
+        </h5>
+
+        <div><h2>Web Develpment</h2></div>
+
+        <div><h5>https://github.com/DavidHostler/Invinzsible</h5></div>
+
+        
+        <h5>
+
+        During 2021, I briefly worked as a Fullstack developer for a business 
+        called Invinzsible Inc. With a small team, I implemented many (but not all)
+        of the website's features using my favorite stack, ReactJS and the Django
+        Rest Framework.
+
+        Using ReactJS, I built a basic layout of page navigation to get from the homse
+        page to the checkout and inventory screens. Additionally, I implemented a backend
+        using Django with Python and created three endpoints: Products, Orders, and Cart.
+        I deployed this basic REST API to Heroku, and used Postman to make sure that various 
+        HTTP requests worked (GET, POST, PUT, and DELETE).
+        
+        Once the basic CRUD app was working online, I made use of Axios, a very useful web client
+        for NodeJS, and basic React hooks (useState, useEffect) to interact with JSON data from
+        the API. This would allow a potential user to view a product, add it to their cart, and 
+        make a purchase of all items in said cart once their shopping trip is satisfactory.
+
+        </h5>
+         
+         <div><h5>
+             <div>I have developed a ton of other projects related to web and mobile
+                fullstack development, some yet to be made public on Github.
+                Coming soon, I may post the React-Native chat app code that I've been working on
+                for mobile (I was hoping to maybe get a few customers should I complete it!)
+                </div>
+             <div>
+                Additionally, I have a few stock projects that I may have had little time to 
+                complete but that I thought were potential money-makers, including a dating app, 
+                a cryptocurrency payment service, and maybe a data based fitness app.
+             </div>
+             <div>
+                The latter I suspect will be difficult to monetize as a consequence of human hubris-
+                attempting to make accurate recommendations for fitness routines, users would have to give
+                honest measurements of their strength and size. The problem with this is that most 
+                young men will say that they can bench 225 and think that they are 15% bodyfat!
+             </div>
+
+
+                
+            
+                
+
+                
+
+             </h5></div>
+    </div>
+    </div>
+
+    )
+}
+
+export default ProjectsScreen;
